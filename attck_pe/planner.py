3c1e698706c28c7d69f55ab447ae0628c85a44d7
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-10-30 06:16:12 -0500

    modified planner

--- attck_pe/planner.py
+++ attck_pe/planner.py
@@ -1,1 +1,9 @@
-# Map
+from attackcti import attack_client
+
+# Initialize ATT&CK client
+client = attack_client()
+
+# Get all techniques in ATT&CK
+techniques = client.get_techniques()
+
+print(f"Total ATT&CK Techniques: {len(techniques)}")
